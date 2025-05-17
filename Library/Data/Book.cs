89d4dd8c82b24fb4b9c99a88d075d17d8a86f366
KulmerKlara <klara.kulmer@outlook.com>
2025-05-17 14:37:22 +0200

    Matching error fixed

--- Library/Data/Book.cs
+++ Library/Data/Book.cs
@@ -2,16 +2,16 @@ namespace Library.Data;
 
 public class Book
 {
-    public int Id { get; set; }
+    public int BookId { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
     public string Genre { get; set; }
     public string Summary { get; set; }
     public bool IsAvailable { get; set; } = true;
 
-    public Book(int id, string title, string author, string genre, string summary, bool isAvailable = true)
+    public Book(int bookId, string title, string author, string genre, string summary, bool isAvailable = true)
     {
-        Id = id;
+        BookId = bookId;
         Title = title;
         Author = author;
         Genre = genre;
